ddfdff1b23098e8b8ebe822ca8ffe854f7170106
Juan V. Vía <juan.via@gmail.com>
2019-06-03 14:02:00 -0300

    Indents in solve.js

--- solve.js
+++ solve.js
@@ -1,2 +1,14 @@
-import {multiply, transpose, inv} from 'mathjs'
-export default (A,b) => multiply(multiply(inv(multiply(transpose(A),A)),transpose(A)),b)
+import { multiply, transpose, inv } from 'mathjs'
+
+export default (A,b) => multiply(
+  multiply(
+    inv(
+      multiply(
+        transpose(A),
+        A
+      )
+    ),
+    transpose(A)
+  ),
+  b
+)
