ae19535c8543c102bfda96adf18981515432c2b4
Juan V. Vía <juan.via@gmail.com>
2021-08-15 01:18:20 -0300

    Esquema completo, hasta el final

--- ls.js
+++ ls.js
@@ -1,15 +1,22 @@
-const { multiply, transpose, inv, matrix, format } = require( "mathjs")
+const { multiply, transpose, inv, matrix, format } = require("mathjs");
 
 const solve = (A, b) => {
-  const transposed    = transpose(A)
-  const product       = multiply(transposed, A)
-  const inverse       = inv(product)
-  const pseudoInverse = multiply(inverse,transposed)
-  const x             = multiply(pseudoInverse, b)
-  return x
-}
+  const transposed = transpose(A);
+  const product = multiply(transposed, A);
+  const inverse = inv(product);
+  const pseudoInverse = multiply(inverse, transposed);
+  const x = multiply(pseudoInverse, b);
+  return x;
+};
 
-const A = matrix([[2,1],[5,1],[7,1],[11,1],[14,1],[18,1]])
-const b = matrix([5,5,8,7,9,7]);
+// const A = matrix([
+//   [4, 2, 1],
+//   [25, 5, 1],
+//   [49, 7, 1],
+//   [121, 11, 1],
+//   [196, 14, 1],
+//   [324, 18, 1],
+// ]);
+const b = matrix([5, 5, 8, 7, 9, 7]);
 
-console.log(format(solve(A, b),5))
+console.log(format(solve(A, b), 5));
--- points.js
+++ points.js
@@ -1,16 +1,41 @@
-const {range, reverse, map, forEach,identity, curry } = require('ramda')
-const line = (degree, x) => 
-  map(exp=>x**exp, reverse(range(0,degree+1)))
+const {
+  range,
+  reverse,
+  map,
+  forEach,
+  identity,
+  curry,
+  reduce,
+  addIndex,
+} = require("ramda");
+const line = (degree, x) =>
+  map((exp) => x ** exp, reverse(range(0, degree + 1)));
 
-const points = [[2,5],[5,5],[7,8],[11,7],[14,9],[18,7]]
-console.log(line(2,2))
+const points = [
+  [2, 5],
+  [5, 5],
+  [7, 8],
+  [11, 7],
+  [14, 9],
+  [18, 7],
+];
+console.log(line(2, 2));
 const point = curry((degree, p) => {
-  console.log({degree,p})
-})
+  const coeficientes = line(degree, p[0]);
+  const y = p[1];
+  //console.log({ coeficientes, y });
+  console.log(
+    addIndex(reduce)((a, c, i) => {
+      const exponente = degree - i;
+      return `${a}${c !== 1 ? c : ""}a_${exponente}${exponente>0?"+": ""}`;
+    }, "")(coeficientes)+`=${y} \\\\
+    `
+  );
+});
+const deg = (degree) => {
+  console.log(`Degree: ${degree} \\\\
+  `);
+  forEach(point(degree), points);
+};
 
-const deg = degree => {
-  console.log(`Degree: ${degree}`)
-  forEach(point(degree),points)
-}
-
-forEach(deg,range(1,7))
+forEach(deg, range(1, 7));
