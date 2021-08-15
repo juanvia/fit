f738eb663c105f0c387bc133348d1ac064c418c4
Juan V. Vía <juan.via@gmail.com>
2021-08-15 17:15:04 -0300

    Con residual

--- ls.js
+++ ls.js
@@ -1,4 +1,4 @@
-const { multiply, transpose, inv, matrix, format } = require("mathjs");
+const { multiply, subtract, transpose, inv, matrix, format } = require("mathjs");
 
 const solve = (A, b) => {
   const transposed = transpose(A);
@@ -17,6 +17,18 @@ const solve = (A, b) => {
 //   [196, 14, 1],
 //   [324, 18, 1],
 // ]);
-const b = matrix([5, 5, 8, 7, 9, 7]);
 
-console.log(format(solve(A, b), 5));
+const A = matrix([
+  [4, 2, 1],
+  [25, 5, 1],
+  [49, 7, 1],
+  [121, 11, 1],
+  [196, 14, 1],
+  [324, 18, 1],
+]);
+
+const b = matrix([5, 5, 8, 7, 9, 7]);
+const x = solve(A, b);
+console.log(format(x, 5));
+const residual = subtract(multiply(A,x),b)
+console.log(format(residual,5))
