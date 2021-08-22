2e48d2eeefea9897d33240558742b2fce051f684
Juan V. Vía <juan.via@gmail.com>
2021-08-22 02:03:00 -0300

    Ampliación de la explicación

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const { multiply, subtract, transpose, inv, matrix, format } = require("mathjs");
+const {
+  multiply,
+  subtract,
+  transpose,
+  inv,
+  matrix,
+  format,
+} = require("mathjs");
+
+const { sum, zip } = require("ramda");
 
 const solve = (A, b) => {
   const transposed = transpose(A);
@@ -30,7 +39,20 @@ const A = matrix([
 const b = matrix([5, 5, 8, 7, 9, 7]);
 const x = solve(A, b);
 console.log(format(x, 5));
-const residual = subtract(multiply(A,x),b)
-const squares = residual.map(x => x*x)
-console.log(format(residual,5))
-console.log(format(squares,5))
+const residual = subtract(multiply(A, x), b);
+const squares = residual.map((x) => x * x);
+console.log(format(residual, 5));
+console.log(format(squares, 5));
+
+// const xs = [2, 5, 7, 11, 14, 18];
+// const ys = [5, 5, 8, 7, 9, 7];
+
+// const y = (x) => 0.171831 * x + 5.20094;
+
+// const otherYs = xs.map(y);
+
+// const yDifferences = zip(ys, otherYs).map(([y1, y2]) => y1 - y2);
+
+// const cost = sum(yDifferences);
+
+// console.log(yDifferences);
