9e0b1bd6edee74505935b2a550c861bfabb2852f
Juan V. Vía <juan.via@gmail.com>
2021-08-25 17:34:02 -0300

    Starting multi

--- ls.js
+++ ls.js
@@ -18,16 +18,22 @@ const solve = (A, b) => {
 //   [324, 18, 1],
 // ]);
 
+// const A = matrix([
+//   [4, 2, 1],
+//   [25, 5, 1],
+//   [49, 7, 1],
+//   [121, 11, 1],
+//   [196, 14, 1],
+//   [324, 18, 1],
+// ]);
 const A = matrix([
-  [4, 2, 1],
-  [25, 5, 1],
-  [49, 7, 1],
-  [121, 11, 1],
-  [196, 14, 1],
-  [324, 18, 1],
+  [-1,1,0],
+  [0,1,-1],
+  [1,1,1],
 ]);
 
-const b = matrix([5, 5, 8, 7, 9, 7]);
+//const b = matrix([5, 5, 8, 7, 9, 7]);
+const b = matrix([3,8,25]);
 const x = solve(A, b);
 console.log(format(x, 5));
 const residual = subtract(multiply(A,x),b)
